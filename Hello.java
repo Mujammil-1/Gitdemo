cd92f7175dc768fbcb2d1a59108a436526ae9e44
Mujammil-1 <mujammilmulla806@gmail.com>
2026-08-17 00:42:34 +0530

    Updated Code

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String args[]) {
         System.out.println("Welcome to Jenkins");
+        System.out.println("Automatic Build Triggered");
+    }
+}
     }
 }
